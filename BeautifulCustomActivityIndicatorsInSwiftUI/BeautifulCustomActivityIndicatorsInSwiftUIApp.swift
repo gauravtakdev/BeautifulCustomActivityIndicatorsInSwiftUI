3bfca8c03e8d4b5903aebe74b0e9071d4c8d2806
Gaurav Tak <gaurav.tak@roro.io>
2025-11-17 14:27:15 +0530

    initial files added

---
+++ BeautifulCustomActivityIndicatorsInSwiftUI/BeautifulCustomActivityIndicatorsInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeautifulCustomActivityIndicatorsInSwiftUIApp.swift
+//  BeautifulCustomActivityIndicatorsInSwiftUI
+//
+//  Created by Gaurav Tak on 17/11/25.
+//
+
+import SwiftUI
+
+@main
+struct BeautifulCustomActivityIndicatorsInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeautifulCustomActivityIndicatorsInSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BeautifulCustomActivityIndicatorsInSwiftUI
+//
+//  Created by Gaurav Tak on 17/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
