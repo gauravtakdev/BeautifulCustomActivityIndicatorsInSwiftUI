fc1838e9c0f7705285e96a148fdd498b93eb11fb
Gaurav Tak <gaurav.tak@roro.io>
2025-11-18 02:15:34 +0530

    loading overlay added with every activity indicators animations

---
+++ BeautifulCustomActivityIndicatorsInSwiftUI/LoaderType.swift
@@ -0,0 +1,9 @@
+enum LoaderType {
+    case ExpandingHexagonLoader
+    case MorphingPolygonLoader
+    case RotatingSquaresLoader
+    case RotatingTriangleLoader
+    case SpinningSquaresRingLoader
+    case TwistingDiamondLoader
+    
+}
