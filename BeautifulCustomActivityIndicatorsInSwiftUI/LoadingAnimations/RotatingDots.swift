cc80d422103637ed245819e97516528628a67053
Gaurav Tak <gaurav.tak@roro.io>
2025-11-18 01:43:25 +0530

    RotatingDots animation added

---
+++ BeautifulCustomActivityIndicatorsInSwiftUI/LoadingAnimations/RotatingDots.swift
@@ -0,0 +1,29 @@
+//
+//  RotatingDots.swift
+//  BeautifulCustomActivityIndicatorsInSwiftUI
+//
+//  Created by Gaurav Tak on 18/11/25.
+//
+
+
+struct RotatingDots: View {
+    @State private var rotate = false
+    
+    var body: some View {
+        HStack(spacing: 8) {
+            ForEach(0..<5) { index in
+                Circle()
+                    .frame(width: 16, height: 16)
+                    .foregroundStyle(.blue)
+                    .scaleEffect(rotate ? 1 : 0.5)
+                    .animation(
+                        .easeInOut(duration: 0.6)
+                            .repeatForever()
+                            .delay(Double(index) * 0.2),
+                        value: rotate
+                    )
+            }
+        }
+        .onAppear { rotate = true }
+    }
+}
